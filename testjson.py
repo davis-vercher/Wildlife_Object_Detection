8d930440a1f478489898ae53f84e44c1019c2232
Davis Vercher <davisvercher@gmail.com>
2026-03-10 12:06:47 -0500

    Final updatest to initial test script functionality

--- testjson.py
+++ testjson.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 import os
 
 tasks_file = r"C:\Users\Personal\Documents\Wildlife Object Detection Project 2025-2026\tasks.json"
-labels_folder = r"C:\Users\Personal\Documents\Wildlife Object Detection Project 2025-2026\Archive\project-1-at-2026-03-01-12-41-76bc6933\labels"
+labels_folder = r"C:\Users\Personal\Documents\Wildlife Object Detection Project 2025-2026\Archive\project-1-at-2026-03-01-12-41-76bc6933\test_labels"
 
 # Load the JSON file
 with open(tasks_file, "r") as f:
@@ -24,19 +24,25 @@ for task in tasks:
     elif len(raw_task_id) == 1:
         task_id = str("000" + raw_task_id)
 
-    image_name = str((task["data"]["image"])[-8:])
+    image_name = str((task["data"]["image"])[-8:-4])
     
     res[task_id].append(image_name)
 
 
 # Process every label file
-for file in os.listdir(labels_folder)[:10]:
+for file in os.listdir(labels_folder):
 
     if not file.endswith(".txt"):
         continue
 
     file_name = file[-8:-4]
-    
-    new_name = res[file_name]
+    new_name = res[file_name][0] + ".txt"
 
-    print(file_name), print(new_name)
+    old_path = os.path.join(labels_folder, file)
+    new_path = os.path.join(labels_folder, new_name)
+
+    if not os.path.exists(new_path):
+        os.rename(old_path, new_path)
+        print(file, " --> ", new_name)
+    else:
+        print("Skipping (already exists): ", new_name)
