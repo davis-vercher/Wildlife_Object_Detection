a25a5369d2bb01c1c6fea4df1b69628a429099fa
Dave Vercher <davisvercher@gmail.com>
2026-06-13 10:32:55 -0500

    Created individual file to test GPU connection and CUDA availability

---
+++ gpu_cuda_test.py
@@ -0,0 +1,6 @@
+# Below is for testing CUDA availabilty/integration with PyTorch for local machine
+import torch
+
+print(f"Is CUDA available?: {torch.cuda.is_available()}")
+print(f"PyTorch CUDA version: {torch.version.cuda}")
+print(f"Device Name: {torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'None'}")
--- model_train.py
+++ model_train.py
@@ -1,12 +1,3 @@
-"""
-# Below is for testing CUDA availabilty/integration with PyTorch for local machine
-import torch
-
-print(f"Is CUDA available?: {torch.cuda.is_available()}")
-print(f"PyTorch CUDA version: {torch.version.cuda}")
-print(f"Device Name: {torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'None'}")
-"""
-
 from ultralytics import YOLO
 
 # Load a pretrained YOLO model
