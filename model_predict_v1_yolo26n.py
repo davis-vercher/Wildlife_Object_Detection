27f2136722cc873e891b468454da5a692b1c16ac
Dave Vercher <davisvercher@gmail.com>
2026-06-22 14:24:49 -0500

    Simplifying single image prediction .py file

--- model_predict_v1_yolo26n.py
+++ model_predict_v1_yolo26n.py
@@ -2,8 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO(r"C:\Users\Dave\Documents\robotics_project\Wildlife_Object_Detection\runs\detect\wildlife_yolo26n\weights\best.pt")
 
-model.predict(
-    source=r"C:\Users\Dave\Desktop\0037.jpg",
-    conf=0.25,
-    save=True
-)
+results = model("C:/Users/Dave/Desktop/pig.jpg")
+
+for result in results:
+    result.show()
